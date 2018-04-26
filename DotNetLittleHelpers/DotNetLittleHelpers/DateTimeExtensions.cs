61618bd646f7e44b2ba9d20db401cce30ce0d85f
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-04-26 09:43:47 +0200

    Add first methods

--- DotNetLittleHelpers/DotNetLittleHelpers/DateTimeExtensions.cs
+++ DotNetLittleHelpers/DotNetLittleHelpers/DateTimeExtensions.cs
@@ -11,5 +11,36 @@ namespace DotNetLittleHelpers
     /// </summary>
     public static class DateTimeExtensions
     {
+        /// <summary>
+        /// Gets the first day of the week of the specified date time
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static DateTime FirstDayOfWeek(this DateTime dt)
+        {
+            return dt.FirstDayOfWeek(DayOfWeek.Monday);
+        }
+
+        /// <summary>
+        /// Gets the first day of the week of the specified date time (using the specified day as 'first day'
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="startOfWeekDay"></param>
+        /// <returns></returns>
+        public static DateTime FirstDayOfWeek(this DateTime dt, DayOfWeek startOfWeekDay)
+        {
+            int diff = (7 + (dt.DayOfWeek - startOfWeekDay)) % 7;
+            return dt.AddDays(-1 * diff).Date;
+        }
+
+        /// <summary>
+        /// Gets the last day of the week of the specified date time
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static DateTime LastDayOfWeek(this DateTime dt)
+        {
+            return dt.FirstDayOfWeek().AddDays(6);
+        }
     }
 }
