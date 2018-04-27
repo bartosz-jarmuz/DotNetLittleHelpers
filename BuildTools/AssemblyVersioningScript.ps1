a97bfbd28964bd7cf038890feb683670f6606565
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-04-27 11:25:06 +0200

    Use different script

--- BuildTools/AssemblyVersioningScript.ps1
+++ BuildTools/AssemblyVersioningScript.ps1
@@ -1,50 +1,91 @@
-function Update-AssemblyInfoVersionFile
-{
-    Param
-    (
-	[Parameter(Mandatory=$true)]
-        [string]$productVersion
-    )
-     
-    $buildNumber = $env:BUILD_BUILDNUMBER
-    if ($buildNumber -eq $null)
-    {
-        $buildIncrementalNumber = 0
-    }
-    else
-    {
-        $splitted = $buildNumber.Split('.')
-        $buildIncrementalNumber = $splitted[$splitted.Length - 1]
-    }
-      
-    $SrcPath = $env:BUILD_SOURCESDIRECTORY
-    Write-Verbose "Executing Update-AssemblyInfoVersionFiles in path $SrcPath for product version Version $productVersion"  -Verbose
- 
-    $AllVersionFiles = Get-ChildItem $SrcPath AssemblyInfo.cs -recurse
-	
-    $versions = $productVersion.Split('.')
-    $major = $versions[0]
-    $minor = $versions[1]
-    $patch = $versions[2]
+##-----------------------------------------------------------------------
+## <copyright file="ApplyVersionToAssemblies.ps1">(c) Microsoft Corporation. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
+##-----------------------------------------------------------------------
+# Look for a 0.0.0.0 pattern in the build number. 
+# If found use it to version the assemblies.
+#
+# For example, if the 'Build number format' build process parameter 
+# $(BuildDefinitionName)_$(Year:yyyy).$(Month).$(DayOfMonth)$(Rev:.r)
+# then your build numbers come out like this:
+# "Build HelloWorld_2013.07.19.1"
+# This script would then apply version 2013.07.19.1 to your assemblies.
 
-    $assemblyVersion = $productVersion
-    $assemblyFileVersion = "$major.$minor.$patch.$buildIncrementalNumber"
-    $assemblyInformationalVersion = $productVersion
-     
-    Write-Verbose "Transformed Assembly Version is $assemblyVersion" -Verbose
-    Write-Verbose "Transformed Assembly File Version is $assemblyFileVersion" -Verbose
-    Write-Verbose "Transformed Assembly Informational Version is $assemblyInformationalVersion" -Verbose
- 
-    foreach ($file in $AllVersionFiles) 
-    { 
-        (Get-Content $file.FullName) |
-        %{$_ -replace 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', "AssemblyVersion(""$assemblyVersion"")" } |
-        %{$_ -replace 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', "AssemblyFileVersion(""$assemblyFileVersion"")" } |
-	%{$_ -replace 'AssemblyInformationalVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)', "AssemblyInformationalVersion(""$assemblyInformationalVersion"")" } | 
-	Set-Content $file.FullName -Force
-    }
-  
-    return $assemblyFileVersion
+# Enable -Verbose option
+[CmdletBinding()]
+
+# Regular expression pattern to find the version in the build number 
+# and then apply it to the assemblies
+$VersionRegex = "\d+\.\d+\.\d+\.\d+"
+
+# If this script is not running on a build server, remind user to 
+# set environment variables so that this script can be debugged
+if(-not ($Env:BUILD_SOURCESDIRECTORY -and $Env:BUILD_BUILDNUMBER))
+{
+    Write-Error "You must set the following environment variables"
+    Write-Error "to test this script interactively."
+    Write-Host '$Env:BUILD_SOURCESDIRECTORY - For example, enter something like:'
+    Write-Host '$Env:BUILD_SOURCESDIRECTORY = "C:\code\FabrikamTFVC\HelloWorld"'
+    Write-Host '$Env:BUILD_BUILDNUMBER - For example, enter something like:'
+    Write-Host '$Env:BUILD_BUILDNUMBER = "Build HelloWorld_0000.00.00.0"'
+    exit 1
 }
 
-Update-AssemblyInfoVersionFile $args[0]
+# Make sure path to source code directory is available
+if (-not $Env:BUILD_SOURCESDIRECTORY)
+{
+    Write-Error ("BUILD_SOURCESDIRECTORY environment variable is missing.")
+    exit 1
+}
+elseif (-not (Test-Path $Env:BUILD_SOURCESDIRECTORY))
+{
+    Write-Error "BUILD_SOURCESDIRECTORY does not exist: $Env:BUILD_SOURCESDIRECTORY"
+    exit 1
+}
+Write-Verbose "BUILD_SOURCESDIRECTORY: $Env:BUILD_SOURCESDIRECTORY"
+
+# Make sure there is a build number
+if (-not $Env:BUILD_BUILDNUMBER)
+{
+    Write-Error ("BUILD_BUILDNUMBER environment variable is missing.")
+    exit 1
+}
+Write-Verbose "BUILD_BUILDNUMBER: $Env:BUILD_BUILDNUMBER"
+
+# Get and validate the version data
+$VersionData = [regex]::matches($Env:BUILD_BUILDNUMBER,$VersionRegex)
+switch($VersionData.Count)
+{
+   0        
+      { 
+         Write-Error "Could not find version number data in BUILD_BUILDNUMBER."
+         exit 1
+      }
+   1 {}
+   default 
+      { 
+         Write-Warning "Found more than instance of version data in BUILD_BUILDNUMBER." 
+         Write-Warning "Will assume first instance is version."
+      }
+}
+$NewVersion = $VersionData[0]
+Write-Verbose "Version: $NewVersion"
+
+# Apply the version to the assembly property files
+$files = gci $Env:BUILD_SOURCESDIRECTORY -recurse -include "*Properties*","My Project" | 
+    ?{ $_.PSIsContainer } | 
+    foreach { gci -Path $_.FullName -Recurse -include AssemblyInfo.* }
+if($files)
+{
+    Write-Verbose "Will apply $NewVersion to $($files.count) files."
+
+    foreach ($file in $files) {
+        $filecontent = Get-Content($file)
+        attrib $file -r
+        $filecontent -replace $VersionRegex, $NewVersion | Out-File $file
+        Write-Verbose "$file.FullName - version applied"
+    }
+}
+else
+{
+    Write-Warning "Found no files."
+}
