378e84e8c7593b9c01f06fa99f65d9c92ec8bb0e
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-05-14 15:33:57 +0200

    Add email validator

---
+++ DotNetLittleHelpers/DotNetLittleHelpers.Tests/StringExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetLittleHelpers.Tests
+{
+    [TestClass()]
+    public class StringExtensionsTests
+    {
+
+        [TestMethod()]
+        public void ValidateEmailTest()
+        {
+            Assert.IsTrue("david.jones@proseware.com".IsValidEmail());
+            Assert.IsTrue("d.j@server1.proseware.com".IsValidEmail());
+            Assert.IsTrue("jones@ms1.proseware.com".IsValidEmail());
+            Assert.IsTrue("js@proseware.com9".IsValidEmail());
+            Assert.IsTrue("j.s@server1.proseware.com".IsValidEmail());
+            Assert.IsTrue("\"j\\\"s\\\"\"@proseware.com".IsValidEmail());
+            Assert.IsTrue("js@contoso.中国".IsValidEmail());
+            Assert.IsTrue("j@proseware.com9".IsValidEmail());
+            Assert.IsTrue("js#internal@proseware.com".IsValidEmail());
+            Assert.IsTrue("j_9@[129.126.118.1]".IsValidEmail());
+
+            Assert.IsFalse("j..s@proseware.com".IsValidEmail());
+            Assert.IsFalse("js*@proseware.com".IsValidEmail());
+            Assert.IsFalse("js@proseware..com".IsValidEmail());
+            Assert.IsFalse("j.@server1.proseware.com".IsValidEmail());
+            Assert.IsFalse("notemail".IsValidEmail());
+            Assert.IsFalse("not.email".IsValidEmail());
+
+
+
+        }
+    }
+}
---
+++ DotNetLittleHelpers/DotNetLittleHelpers/StringExtensions.cs
@@ -0,0 +1,66 @@
+﻿namespace DotNetLittleHelpers
+{
+    using System;
+    using System.Globalization;
+    using System.Text.RegularExpressions;
+
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Verifies whether a string is a valid email address (Microsoft way)
+        /// <para>https://docs.microsoft.com/en-us/dotnet/standard/base-types/how-to-verify-that-strings-are-in-valid-email-format</para>
+        /// </summary>
+        /// <param name="maybeEmail"></param>
+        /// <returns></returns>
+        public static bool IsValidEmail(this string maybeEmail)
+        {
+            bool invalid = false;
+            if (String.IsNullOrEmpty(maybeEmail))
+                return false;
+
+            // Use IdnMapping class to convert Unicode domain names.
+            try
+            {
+                maybeEmail = Regex.Replace(maybeEmail, @"(@)(.+)$", x=> DomainMapper(x, ref invalid),
+                    RegexOptions.None, TimeSpan.FromMilliseconds(200));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+
+            if (invalid)
+                return false;
+
+            // Return true if strIn is in valid email format.
+            try
+            {
+                return Regex.IsMatch(maybeEmail,
+                    @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                    @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        private static string DomainMapper(Match match, ref bool invalid)
+        {
+            // IdnMapping class with default property values.
+            IdnMapping idn = new IdnMapping();
+
+            string domainName = match.Groups[2].Value;
+            try
+            {
+                domainName = idn.GetAscii(domainName);
+            }
+            catch (ArgumentException)
+            {
+                invalid = true;
+            }
+            return match.Groups[1].Value + domainName;
+        }
+    }
+}
