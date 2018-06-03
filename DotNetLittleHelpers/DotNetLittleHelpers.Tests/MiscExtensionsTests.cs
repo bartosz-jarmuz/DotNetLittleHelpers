62f429a5b57604d623ec56e3822ac2bb69997d7e
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-06-03 17:56:00 +0200

    Split test

--- DotNetLittleHelpers/DotNetLittleHelpers.Tests/MiscExtensionsTests.cs
+++ DotNetLittleHelpers/DotNetLittleHelpers.Tests/MiscExtensionsTests.cs
@@ -149,9 +149,15 @@ namespace DotNetLittleHelpers.Tests
             PropertyStringTestObject obj = new PropertyStringTestObject();
             Assert.AreEqual($"Number: [0], Name: [*NULL*], SomeBoolean: [False], Id: [00000000-0000-0000-0000-000000000000], NestedObject: [*NULL*]",
                 obj.GetPropertyInfoString(nameof(PropertyStringTestObject.Number), nameof(PropertyStringTestObject.Name), nameof(PropertyStringTestObject.SomeBoolean), nameof(PropertyStringTestObject.Id), nameof(PropertyStringTestObject.NestedObject)));
+        }
 
+        [TestMethod()]
+        public void TestGetPropertyStringAllProps_Nulls()
+        {
+            PropertyStringTestObject obj = new PropertyStringTestObject();
+           
             Assert.AreEqual($"Number: [0], Name: [*NULL*], SomeBoolean: [False], Id: [00000000-0000-0000-0000-000000000000], NestedObject: [*NULL*]",
-                obj.GetPropertyInfoString()); 
+                obj.GetPropertyInfoString());
         }
 
     }
