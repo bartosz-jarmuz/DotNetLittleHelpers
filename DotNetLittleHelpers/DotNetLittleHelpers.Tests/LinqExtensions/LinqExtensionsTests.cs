153676733e23c68f6d8779a0309d886b81b983a4
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-06-12 22:41:00 +0200

    Add 3 'is null' extensions

--- DotNetLittleHelpers/DotNetLittleHelpers.Tests/LinqExtensions/LinqExtensionsTests.cs
+++ DotNetLittleHelpers/DotNetLittleHelpers.Tests/LinqExtensions/LinqExtensionsTests.cs
@@ -47,11 +47,53 @@ namespace DotNetLittleHelpers.Tests
                 return;
             }
             Assert.Fail("Exception expected");
-
-
-
         }
 
-     
+        [Test]
+        public void AnyAndNotNull()
+        {
+            List<TestObject> list = null;
+            Assert.IsFalse(list.AnyAndNotNull());
+
+            list = new List<TestObject>();
+            Assert.IsFalse(list.AnyAndNotNull());
+
+            list = new List<TestObject> {null};
+            Assert.IsTrue(list.AnyAndNotNull());
+
+            list = new List<TestObject> { new TestObject() };
+            Assert.IsTrue(list.AnyAndNotNull());
+        }
+
+
+        [Test]
+        public void Enumerable_IsNullOrEmpty()
+        {
+            List<TestObject> list = null;
+            Assert.IsTrue(list.IsNullOrEmpty());
+
+            list = new List<TestObject>();
+            Assert.IsTrue(list.IsNullOrEmpty());
+
+            list = new List<TestObject> { null };
+            Assert.IsFalse(list.IsNullOrEmpty());
+
+            list = new List<TestObject> { new TestObject() };
+            Assert.IsFalse(list.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void IsNullOrDefault()
+        {
+            Guid? nullableGuid = null;
+            Assert.IsTrue(nullableGuid.IsNullOrDefault());
+
+            nullableGuid = Guid.Empty;
+            Assert.IsTrue(nullableGuid.IsNullOrDefault());
+
+            nullableGuid = Guid.NewGuid();
+            Assert.IsFalse(nullableGuid.IsNullOrDefault());
+        }
+
     }
 }
--- DotNetLittleHelpers/DotNetLittleHelpers/LinqExtensions/MiscLinqExtensions.cs
+++ DotNetLittleHelpers/DotNetLittleHelpers/LinqExtensions/MiscLinqExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace DotNetLittleHelpers
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
     using System.Linq.Expressions;
@@ -13,6 +14,47 @@
     /// </summary>
     public static class LinqExtensions
     {
+        /// <summary>
+        /// Returns false if collection is null or empty<para/>
+        /// Syntactic sugar for if (source != null &amp;&amp; source.Any())
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool AnyAndNotNull<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                return false;
+            }
+            else
+            {
+                return source.Any();
+            }
+        }
+
+        /// <summary>
+        /// Returns true if collection is null or empty<para/>
+        /// Syntactic sugar for if (source == null || !source.Any())
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return !source.AnyAndNotNull();
+        }
+
+        /// <summary>
+        /// Returns true if a value type is either null or a default for that type (default(T))
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool IsNullOrDefault<T>(this T? source) where T : struct
+        {
+            return !source.HasValue || source.Value.Equals(default(T));
+        }
 
         /// <summary>
         /// Throws an ArgumentNullException if the argument is null
