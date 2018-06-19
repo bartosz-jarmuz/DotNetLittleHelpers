d7008b0e0dee3986f6201048fa9abd6d7496ac80
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-06-19 13:46:48 +0200

    Add cutom version comparer

--- DotNetLittleHelpers/DotNetLittleHelpers.Tests/StringExtensionsTests.cs
+++ DotNetLittleHelpers/DotNetLittleHelpers.Tests/StringExtensionsTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DotNetLittleHelpers.Tests
 {
@@ -66,6 +68,34 @@ namespace DotNetLittleHelpers.Tests
         {
             NUnit.Framework.Assert.That(() => "0".IsNewerVersionThan(null), Throws.Exception.With.Message.Contain("Null parameter passed to method [IsNewerVersionThan]"));
             NUnit.Framework.Assert.That(() => "0".IsNewerVersionThan("0.9.0"), Throws.Exception.With.Message.Contain("Error while parsing [0] as Version"));
+
+        }
+
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestVersionComparer_List()
+        {
+            var list = new List<string>()
+            {
+                "1.0.0.0",
+                "0.9.1",
+                "2.0",
+                "0.9.0",
+            };
+            var ordered = list.OrderBy(x => x, new VersionStringComparer()).ToList();
+
+            Assert.AreEqual("0.9.0", ordered[0]);
+            Assert.AreEqual("0.9.1", ordered[1]);
+            Assert.AreEqual("1.0.0.0", ordered[2]);
+            Assert.AreEqual("2.0", ordered[3]);
+
+            ordered = list.OrderByDescending(x => x, new VersionStringComparer()).ToList();
+
+            Assert.AreEqual("0.9.0", ordered[3]);
+            Assert.AreEqual("0.9.1", ordered[2]);
+            Assert.AreEqual("1.0.0.0", ordered[1]);
+            Assert.AreEqual("2.0", ordered[0]);
+
 
         }
     }
---
+++ DotNetLittleHelpers/DotNetLittleHelpers/Comparers/VersionStringComparer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetLittleHelpers
+{
+    /// <summary>
+    /// Compares version strings
+    /// </summary>
+    public class VersionStringComparer : IComparer<string>
+    {
+        /// <summary>
+        /// Returns 1 if first version is larger, -1 if version is smaller and 0 if they are equal
+        /// </summary>
+        /// <param name="firstOne"></param>
+        /// <param name="secondOne"></param>
+        /// <returns></returns>
+        public int Compare(string firstOne, string secondOne)
+        {
+            if (firstOne.IsNewerVersionThan(secondOne))
+            {
+                return 1;
+            }
+            else if (firstOne == secondOne)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+    }
+
+}
