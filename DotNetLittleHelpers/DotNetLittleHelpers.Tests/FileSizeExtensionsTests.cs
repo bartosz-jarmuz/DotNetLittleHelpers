b6e28675539a07a30f8cc76ed17296b9f8b1b776
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-07-17 22:45:37 +0200

    Add file size calculations

---
+++ DotNetLittleHelpers/DotNetLittleHelpers.Tests/FileSizeExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotNetLittleHelpers.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Assert = NUnit.Framework.Assert;
+
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass()]
+    public class FileSizeExtensionsTests
+    {
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestGetSizeString()
+        {
+            Assert.AreEqual("0.0 bytes", ((long)0).GetSizeString());
+            Assert.AreEqual("500.0 bytes", ((long)500).GetSizeString());
+            Assert.AreEqual("425.1 KB", ((long)435343).GetSizeString());
+            Assert.AreEqual("425.14 KB", ((long)435343).GetSizeString(2));
+            Assert.AreEqual("8,192.0 PB", (long.MaxValue).GetSizeString());
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestConversionToKilo()
+        {
+            Assert.AreEqual(0, ((long)0).ConvertBytesToKilobytes());
+            Assert.AreEqual(0.25, ((long)256).ConvertBytesToKilobytes());
+            Assert.AreEqual(1, ((long)1024).ConvertBytesToKilobytes());
+            Assert.AreEqual(425.14, ((long)435343).ConvertBytesToKilobytes());
+            Assert.AreEqual(425.13965, ((long)435343).ConvertBytesToKilobytes(5));
+        }
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestConversionToMega()
+        {
+            Assert.AreEqual(0, ((long)0).ConvertBytesToMegabytes());
+            Assert.AreEqual(1, ((long)1024*1024).ConvertBytesToMegabytes());
+            Assert.AreEqual(1.00195, ((long)1024*1024+2048).ConvertBytesToMegabytes(5));
+        }
+
+    }
+}
---
+++ DotNetLittleHelpers/DotNetLittleHelpers/FileSizeExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DotNetLittleHelpers
+{
+    using System.Globalization;
+
+    /// <summary>
+    /// Extensions for calculating file size
+    /// </summary>
+    public static class FileSizeExtensions
+    {
+        /// <summary>
+        /// Converts bytes to kilobytes
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="decimalPlaces"></param>
+        /// <returns></returns>
+        public static double ConvertBytesToKilobytes(this long bytes, int decimalPlaces = 2)
+        {
+            return Math.Round(bytes / 1024f, decimalPlaces, MidpointRounding.AwayFromZero);
+        }
+
+        /// <summary>
+        /// Converts bytes to megabytes
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="decimalPlaces"></param>
+        /// <returns></returns>
+        public static double ConvertBytesToMegabytes(this long bytes, int decimalPlaces = 2)
+        {
+            return Math.Round(bytes / 1024f / 1024f, decimalPlaces, MidpointRounding.AwayFromZero);
+        }
+
+        static readonly string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+        /// <summary>
+        /// Gets the file / folder size string in largest unit (KB, MB, GB etc)
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="decimalPlaces">The decimal places.</param>
+        /// <returns>System.String.</returns>
+        public static string GetSizeString(this Int64 value, int decimalPlaces = 1)
+        {
+            if (value < 0) { return "-" + GetSizeString(-value); }
+
+            int i = 0;
+            decimal dValue = (decimal)value;
+            while (Math.Round(dValue, decimalPlaces) >= 1000 && i < 5)
+            {
+                dValue /= 1024;
+                i++;
+            }
+
+            return string.Format(CultureInfo.InvariantCulture, "{0:n" + decimalPlaces + "} {1}", dValue, SizeSuffixes[i]);
+        }
+    }
+}
