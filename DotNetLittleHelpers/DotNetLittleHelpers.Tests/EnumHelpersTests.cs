234978b250ca434d5874106fa95828dfe31f0446
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-10-07 14:44:05 +0200

    Add enum helpers

---
+++ DotNetLittleHelpers/DotNetLittleHelpers.Tests/EnumHelpersTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetLittleHelpers.Tests
+{
+    using System;
+    using System.Reflection.Emit;
+
+    [TestClass()]
+    public class EnumHelpersTests
+    {
+        private enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        [TestMethod]
+        public void TestIncrease()
+        {
+            Assert.AreEqual(WeekDays.Friday, EnumHelper.IncreaseValue(WeekDays.Tuesday, 3));
+            Assert.AreEqual(WeekDays.Tuesday, EnumHelper.IncreaseValue(WeekDays.Tuesday, 0));
+            Assert.AreEqual(WeekDays.Sunday, EnumHelper.IncreaseValue(WeekDays.Tuesday, 22));
+            try
+            {
+                EnumHelper.IncreaseValue(WeekDays.Tuesday, 22, true);
+                Assert.Fail("Error expected");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "The modifier 22 exceeds the maximum possible value for WeekDays when applied on Tuesday");
+            }
+        }
+
+
+
+        [TestMethod]
+        public void TestDecrease()
+        {
+            Assert.AreEqual(WeekDays.Tuesday, EnumHelper.DecreaseValue(WeekDays.Friday, 3));
+            Assert.AreEqual(WeekDays.Tuesday, EnumHelper.DecreaseValue(WeekDays.Tuesday, 0));
+            Assert.AreEqual(WeekDays.Monday, EnumHelper.DecreaseValue(WeekDays.Tuesday, 22));
+            try
+            {
+                EnumHelper.DecreaseValue(WeekDays.Tuesday, 22, true);
+                Assert.Fail("Error expected");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "The modifier 22 exceeds the minimum possible value for WeekDays when applied on Tuesday");
+            }
+        }
+    }
+}
---
+++ DotNetLittleHelpers/DotNetLittleHelpers/EnumHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetLittleHelpers
+{
+    /// <summary>
+    /// Class EnumHelper.
+    /// </summary>
+    public class EnumHelper
+    {
+        /// <summary>
+        /// Decreases the value of an enum by the specified number of steps
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="theEnum">The enum.</param>
+        /// <param name="modifier">The modifier.</param>
+        /// <param name="throwIfMinValueExceeded">If set to true, if the modified value exceeds the maximum value of the enum, an error is thrown. Otherwise max is returned</param>
+        /// <returns>T.</returns>
+        /// <exception cref="ArgumentException">The requested type [{typeof(T).Name}</exception>
+        public static T DecreaseValue<T>(T theEnum, uint modifier, bool throwIfMinValueExceeded = false) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException($"The requested type [{typeof(T).Name}] is not an enum!");
+            }
+
+            T last = Enum.GetValues(typeof(T)).Cast<T>().Min();
+            int lastValue = Convert.ToInt32(last);
+            uint originalModifier = modifier;
+            int value = (Convert.ToInt32(theEnum) - (int)modifier);
+
+            while (!Enum.IsDefined(typeof(T), value))
+            {
+                if (throwIfMinValueExceeded)
+                {
+                    if (value < lastValue)
+                    {
+                        throw new Exception($"The modifier {originalModifier} exceeds the minimum possible value for {typeof(T).Name} when applied on {theEnum}");
+                    }
+                }
+                else
+                {
+                    if (value <= lastValue)
+                    {
+                        return last;
+                    }
+                }
+                modifier--;
+                value = (Convert.ToInt32(theEnum) + (int)modifier);
+            }
+
+            return (T)Enum.Parse(typeof(T), value.ToString());
+        }
+
+        /// <summary>
+        /// Increases the value of an enum by the specified number of steps
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="theEnum">The enum.</param>
+        /// <param name="modifier">The modifier.</param>
+        /// <param name="throwIfMaxValueExceeded">If set to true, if the modified value exceeds the maximum value of the enum, an error is thrown. Otherwise max is returned</param>
+        /// <returns>T.</returns>
+        /// <exception cref="ArgumentException">The requested type [{typeof(T).Name}</exception>
+        public static T IncreaseValue<T>(T theEnum, uint modifier, bool throwIfMaxValueExceeded = false) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException($"The requested type [{typeof(T).Name}] is not an enum!");
+            }
+
+            int value = (Convert.ToInt32(theEnum) + (int)modifier);
+            uint originalModifier = modifier;
+            T last = Enum.GetValues(typeof(T)).Cast<T>().Max();
+            int lastValue = Convert.ToInt32(last);
+            while (!Enum.IsDefined(typeof(T), value))
+            {
+                if (modifier == 0)
+                {
+                    return theEnum;
+                }
+
+                if (throwIfMaxValueExceeded)
+                {
+                    if (value > lastValue)
+                    {
+                        throw new Exception($"The modifier {originalModifier} exceeds the maximum possible value for {typeof(T).Name} when applied on {theEnum}");
+                    }
+                }
+                else
+                {
+                    if (value >= lastValue)
+                    {
+                        return last;
+                    }
+                }
+
+                modifier--;
+                value = (Convert.ToInt32(theEnum) + (int)modifier);
+            }
+
+            return (T)Enum.Parse(typeof(T), value.ToString());
+        }
+    }
+}
