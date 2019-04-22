cc2f72dfcc363fe88b6c492810891fa12b2fb9a5
Bartosz Jarmuż <bartosz.jarmuz@gmail.com>
2019-04-22 21:26:10 +0000

    Update comment

--- DotNetLittleHelpers/DotNetLittleHelpers/Comparers/TypeExtensions.cs
+++ DotNetLittleHelpers/DotNetLittleHelpers/Comparers/TypeExtensions.cs
@@ -43,10 +43,10 @@
         }
 
         /// <summary>
-        /// Determine whether a type is simple (String, Decimal, DateTime, etc) 
+        /// Determine whether a type is simple (String, Decimal, DateTime, etc)
         /// or complex (i.e. custom class with public properties and methods).
+        /// http://stackoverflow.com/questions/2442534/how-to-test-if-type-is-primitive
         /// </summary>
-        /// <see cref="http://stackoverflow.com/questions/2442534/how-to-test-if-type-is-primitive"/>
         public static bool IsSimpleType(
             this Type type)
         {
