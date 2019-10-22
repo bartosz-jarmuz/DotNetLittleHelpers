45ac3c45e6c224a124c5b8aa341d7395be672fa9
Bartosz Jarmuz <bjarmuz@sdl.com>
2019-10-22 22:52:12 +0200

    add parameter parser Add shouldly for asserts

---
+++ DotNetLittleHelpers/DotNetLittleHelpers.Tests/ParameterClassTests.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Globalization;
+using Shouldly;
+
+namespace DotNetLittleHelpers.Tests
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass()]
+    public class ParameterClassTests
+    {
+        public class PersonParamSet : ParameterSet
+        {
+            public PersonParamSet()
+            {
+            }
+
+            public PersonParamSet(string[] args) : base(args)
+            {
+            }
+
+            public PersonParamSet(string argumentString) : base(argumentString)
+            {
+            }
+
+            public string Name { get; set; }
+            public string LastName { get; set; }
+
+            public string Email { get; set; }
+
+            public int Age { get; set; }
+            public int? NullableOne { get; set; }
+            public int NullableTwo { get; set; }
+            public decimal Weight { get; set; }
+
+            public string Path { get; set; }
+
+            public DateTime RegisteredDate { get; set; }
+
+            public bool Happy { get; set; }
+
+            public bool Drunk { get; set; }
+            public bool Rich { get; set; }
+
+            public void SetPrivateInstanceProperty(bool val)
+            {
+                PrivateInstanceProperty = val;
+            }
+            public bool PrivateInstanceProperty{ get;private  set; }
+
+           public static string PublicStaticProperty { get; set; }
+
+            public string PublicField;
+        }
+
+        static readonly string DecimalSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+        static readonly DateTime DateTimeStamp = DateTime.Now;
+
+        readonly string simplePersonParametersInput = $"-Email john@doe.com -Age 34 --happy -RegisteredDate={DateTimeStamp:O} --drunk -name=\"John \"FunnyGuy\" Doe\" -nullableTwo=666 /Path=\"C:\\My Home\\Living-Room\" --weight=123{DecimalSeparator}43 -NonExistentParameter=HahaNothing";
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void Test_Stringify_RoundTrip()
+        {
+            var person = new PersonParamSet
+            {
+                Weight = 134.2M, 
+                Age = 24,
+                NullableOne = 1,
+                Drunk = true,
+                Email = "jdoe@does.com",
+                Path = "Somewhere Over The Rain...forest",
+                RegisteredDate = new DateTime(1988,02,28)
+            };
+            var stringified = person.SaveAsParameters();
+            var destringified = new PersonParamSet(stringified);
+
+            destringified.ThrowIfPublicPropertiesNotEqual(person);
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void Test_Stringify_RoundTrip_OnePropOnly_TestIgnorable()
+        {
+            var person = new PersonParamSet
+            {
+                Name = "John \"FunnyGuy\" Doe"
+            };
+            person.PublicField = "PublicField Value";
+            PersonParamSet.PublicStaticProperty = "PublicStaticProperty Value";
+            person.SetPrivateInstanceProperty(true);
+
+            var stringified = person.SaveAsParameters();
+            var destringified = new PersonParamSet(stringified);
+
+            destringified.ShouldNotBeSameAs(person);
+            destringified.Name.ShouldBe("John \"FunnyGuy\" Doe");
+            destringified.PublicField.ShouldNotBe(person.PublicField);
+            destringified.PrivateInstanceProperty.ShouldNotBe(person.PrivateInstanceProperty);
+
+            stringified.ShouldBe("--Name=\"John \"FunnyGuy\" Doe\" ");
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void Test_Stringify_RoundTrip_OnePropOnly()
+        {
+            var person = new PersonParamSet
+            {
+                Name = "John \"FunnyGuy\" Doe"
+            };
+
+            var stringified = person.SaveAsParameters();
+
+            var destringified = new PersonParamSet(stringified);
+            destringified.ThrowIfPublicPropertiesNotEqual(person);
+            stringified.ShouldBe("--Name=\"John \"FunnyGuy\" Doe\" ");
+        }
+
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestLoadSimplePerson_LoadMethod()
+        {
+            var args = ParameterSet.Parser.Split(this.simplePersonParametersInput);
+
+            var person = new PersonParamSet();
+            person.LoadParameters(args);
+            AssertSimplePerson(person);
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestLoadSimplePerson_Constructor()
+        {
+            var args = ParameterSet.Parser.Split(this.simplePersonParametersInput);
+            var person = new PersonParamSet(args);
+            AssertSimplePerson(person);
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestLoadSimplePerson_Constructor_FromString()
+        {
+            var person = new PersonParamSet(this.simplePersonParametersInput);
+            AssertSimplePerson(person);
+        }
+
+        private static void AssertSimplePerson(PersonParamSet person)
+        {
+            person.ShouldSatisfyAllConditions(
+                () => person.Email.ShouldBe("john@doe.com"),
+                () => person.Age.ShouldBe(34),
+                () => person.Happy.ShouldBe(true),
+                () => person.RegisteredDate.ShouldBe(DateTimeStamp),
+                () => person.Drunk.ShouldBe(true),
+                () => person.Name.ShouldBe("John \"FunnyGuy\" Doe"),
+                () => person.Path.ShouldBe(@"C:\My Home\Living-Room"),
+                () => person.NullableTwo.ShouldBe(666),
+                () => person.Rich.ShouldBe(false),
+                () => person.LastName.ShouldBe(null),
+                () => person.Weight.ShouldBe(123.43M),
+                () => person.NullableOne.ShouldBe(null)
+            );
+        }
+    }
+}
---
+++ DotNetLittleHelpers/DotNetLittleHelpers.Tests/Strings/ParameterStringsTests.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Shouldly;
+
+namespace DotNetLittleHelpers.Tests
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass()]
+    public class ParameterStringExtensionsTests
+    {
+      
+
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestSplittingWorks()
+        {
+            string input = "-name Bartosz -lastname Jarmuż";
+            string[] output = ParameterSet.Parser.Split(input);
+            output.ShouldBe(new[] {"-name", "Bartosz", "-lastname", "Jarmuż"});
+
+            input = "test --file=\"some file.txt\"";
+            output = ParameterSet.Parser.Split(input);
+            output.ShouldBe(new[] { "test", "--file=\"some file.txt\"" });
+
+            input = @"/src:""C:\tmp\Some Folder\Sub Folder"" /users:""abcdefg@hijkl.com"" tasks:""SomeTask,Some Other Task"" -someParam";
+            output = ParameterSet.Parser.Split(input);
+            output.ShouldBe(new[] { @"/src:""C:\tmp\Some Folder\Sub Folder""", @"/users:""abcdefg@hijkl.com""", @"tasks:""SomeTask,Some Other Task""", "-someParam" });
+
+
+            input = "-name=Bartosz -lastname Jarmuż -emptySpace \" \"";
+            output = ParameterSet.Parser.Split(input);
+            output.ShouldBe(new[] { "-name=Bartosz", "-lastname", "Jarmuż", "-emptySpace", " " });
+           
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestSplittingWorks_Empty()
+        {
+            var output = ParameterSet.Parser.Split("");
+            output.ShouldBe(new string[] { });
+
+            output = ParameterSet.Parser.Split(null);
+            output.ShouldBe(new string[] { });
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestGettingParamsDictionary_Empty()
+        {
+            var output = ParameterSet.Parser.GetParameters(args: null);
+            output.ShouldBe(
+                new List<KeyValuePair<string, string>>()
+                {
+
+                }
+            );
+            output = ParameterSet.Parser.GetParameters(new string[] { });
+            output.ShouldBe(
+                new List<KeyValuePair<string, string>>()
+                {
+
+                }
+            );
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestGettingParamsDictionary_AnonymousParams()
+        {
+            var input = new[] { @"someArg", "someOtherArg" };
+            var output = ParameterSet.Parser.GetParameters(input);
+
+            output.ShouldBe(
+                new List<KeyValuePair<string, string>>()
+                {
+                    new KeyValuePair<string, string>("", "someArg"),
+                    new KeyValuePair<string, string>("", "someOtherArg"),
+                }
+            );
+            var output2 = ParameterSet.Parser.GetParameters(ParameterSet.Parser.Split(string.Join(" ", input)));
+            output2.ShouldBe(output);
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestGettingParamsDictionary_OnlySwitches()
+        {
+            var input = new[] { @"-someSwitch", "-someOtherSwitch" };
+           var  output = ParameterSet.Parser.GetParameters(input);
+
+            output.ShouldBe(
+                new List<KeyValuePair<string, string>>()
+                {
+                    new KeyValuePair<string, string>("someSwitch", "True"),
+                    new KeyValuePair<string, string>("someOtherSwitch", "True"),
+                }
+            );
+            var jointInputString = string.Join(" ", input);
+            var output2 = ParameterSet.Parser.GetParameters(ParameterSet.Parser.Split(jointInputString));
+            output2.ShouldBe(output);
+            var output3 = ParameterSet.Parser.GetParameters(jointInputString);
+            output3.ShouldBe(output);
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod()]
+        public void TestGettingParamsDictionary_AllSortsOfComplexities()
+        {
+            string[] input = new[] { "-name=Bartosz", "-lastname", "Jarmuż", "-emptySpace", "\" \"" };
+            var output = ParameterSet.Parser.GetParameters(input);
+
+            output.ShouldBe(
+                new List<KeyValuePair<string, string>>()
+                {
+                    new KeyValuePair<string, string>("name", "Bartosz"),
+                    new KeyValuePair<string, string>("lastname", "Jarmuż"),
+                    new KeyValuePair<string, string>("emptySpace", " "),
+                }
+            );
+            var jointInputString = string.Join(" ", input);
+            var output2 = ParameterSet.Parser.GetParameters(ParameterSet.Parser.Split(jointInputString));
+            output2.ShouldBe(output);
+            var output3 = ParameterSet.Parser.GetParameters(jointInputString);
+            output3.ShouldBe(output);
+
+            input = new[] { "SomePlainValue",
+                @"-src:""C:\tmp\Some Folder\Sub Folder""",
+                "--someBoolean",
+                @"/users:""abcdefg@hijkl.com""",
+                @"--moreWithSpaces:""There are ""slashes"" /spaces and -hyphens here""",
+                "AnotherPlainValue",
+                "-xmlChunk=\"<ApiResponse Id=\"23\">Error</ApiResponse>\"",
+                @"--tasks:""SomeTask,Some Other Task""",
+                "-someParam" };
+            output = ParameterSet.Parser.GetParameters(input);
+          
+
+            output.ShouldBe(
+                new List<KeyValuePair<string, string>>()
+                {
+                    new KeyValuePair<string, string>("", @"SomePlainValue"),
+                    new KeyValuePair<string, string>("src", @"C:\tmp\Some Folder\Sub Folder"),
+                    new KeyValuePair<string, string>("someBoolean", @"True"),
+                    new KeyValuePair<string, string>("users", "abcdefg@hijkl.com"),
+                    new KeyValuePair<string, string>("moreWithSpaces", "There are \"slashes\" /spaces and -hyphens here"),
+                    new KeyValuePair<string, string>("", "AnotherPlainValue"),
+                    new KeyValuePair<string, string>("xmlChunk", "<ApiResponse Id=\"23\">Error</ApiResponse>"),
+                    new KeyValuePair<string, string>("tasks", "SomeTask,Some Other Task"),
+                    new KeyValuePair<string, string>("someParam", "True"),
+                }
+            );
+
+            jointInputString = string.Join(" ", input);
+            output2 = ParameterSet.Parser.GetParameters(ParameterSet.Parser.Split(jointInputString));
+            output2.ShouldBe(output);
+            output3 = ParameterSet.Parser.GetParameters(jointInputString);
+            output3.ShouldBe(output);
+        }
+
+    }
+}
+
